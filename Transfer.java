b10b6b386c78aadcd80de389107cd216a01814d0
VisualGhost <weatherrocket@gmail.com>
2016-12-26 18:15:17 +0200

    Make variables of Transfer class - private

--- Transfer.java
+++ Transfer.java
@@ -1,5 +1,15 @@
 public class Transfer{
-   long id accountFrom;
-   long id accountTo;
-   double ammount;
+   private long id accountFrom;
+   private long id accountTo;
+   private double ammount;
+   
+   public Transfer(long id accountFrom, long id accountTo, double ammount){
+	   this.accountFrom = accountFrom;
+	   this.accountTo = accountTo;
+	   this.ammount = ammount;
+   }
+   
+   public void transfer(){
+	   // going soon
+   }
 }
